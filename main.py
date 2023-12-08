9255edba854343734fab65f707053cef1ff0a878
Timodey <timo.saratovkin@gfmail.com>
2023-12-08 21:59:45 +0300

    first_commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 800, 600
+    screen = pygame.display.set_mode(size)
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
